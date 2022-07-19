2755563db70dd989d5ea000e68bc7b69566addc4
Wendy You <wyou130@gmail.com>
2022-07-19 18:11:34 -0400

    commit tree 13cf6826460c356567505ad8df95a7b9f243a665

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const names = ['Wendy', 'Ethan', 'Phil', 'Rachel']
+
+const returnFirstTwoDrivers = function(names) {
+    const firstTwoDrivers = names.slice(0,2)
+    return firstTwoDrivers
+}
+
+const returnLastTwoDrivers = function(names) {
+    const lastTwoDrivers = names.slice(-2)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//defining fare multiplying function outside the function scope
+// function fareMultiplying(fare) {
+//     return integer * fare
+// }
+
+// function createFareMultiplier(integer) {
+//     return fareMultiplying()
+// }
+
+//using anonymous function
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return integer * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, oneOfTwoFunctions) {
+    return selectDifferentDrivers(names, firstTwoDrivers())
+}
